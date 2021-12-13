5a716301d22c8f236dc1a0fcdf4afe568b62a10b
dollymridula <92428592+dollymridula@users.noreply.github.com>
2021-12-13 12:52:22 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -50,11 +50,11 @@ function draw() {
 
   playerArcher.display();
 
-    for (var i; i < playerArrows.length; i++) {
-      if (playerArrows[i] !== undefined) {
-        playerArrows[i].display();
-      }
-    }
+   // for (var i; i < playerArrows.length; i++) {
+   //   if (playerArrows[i] !== undefined) {
+   //     playerArrows[i].display();
+   //   }
+   // }
 
     // for (var i = 0, i++) {
     //  if (playerArrows[i] !== undefined) {
@@ -62,11 +62,11 @@ function draw() {
     //  }
     // }
   
-   // for (var i = 0; i < playerArrows.length; i++) {
-   //   if (playerArrows[i] !== undefined) {
-   //     playerArrows[i].display();
-   //   }
-   // }
+    for (var i = 0; i < playerArrows.length; i++) {
+     if (playerArrows[i] !== undefined) {
+        playerArrows[i].display();
+      }
+    }
 
    // for (var i) {
    //   if (playerArrows[i] !== undefined) {
